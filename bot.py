ce1b28cd7e58b97570a0248332ebaf2c906089ef
lxdlam <lxdlam@gmail.com>
2018-05-01 16:06:26 +0800

    Initialize

---
+++ bot.py
@@ -0,0 +1,55 @@
+from random import choice, randint
+import time
+
+commands = dict()
+
+meal = set([
+    "盖浇饭", "砂锅", "大排档", "米线", "满汉全席", "西餐", "麻辣烫", "自助餐", "炒面", "快餐", "水果",
+    "西北风", "馄饨", "火锅", "烧烤", "泡面", "水饺", "日料", "涮羊肉", "味千拉面", "面包", "扬州炒饭",
+    "自助餐", "茶餐厅", "海底捞", "披萨", "金拱门", "KFC", "汉堡王", "卡乐星", "兰州拉面", "沙县小吃",
+    "烤鱼", "烤肉", "海鲜", "铁板烧", "韩国料理", "粥", "快餐", "萨莉亚", "桂林米粉", "东南亚菜", "甜点",
+    "农家菜", "川菜", "粤菜", "湘菜", "生活", "全家", "黄焖鸡米饭", "群主"
+])
+
+
+def register_commands(command):
+    def closure(func):
+        commands[command] = func
+        return func
+
+    return closure
+
+
+def onQQMessage(bot, contact, member, content):
+    if '大床' not in contact.nick or bot.isMe(contact, member):
+        return
+    msg = ''
+    if '帮助' in content:
+        msg += "这是现在可以公布的情报："
+        for k, v in commands.items():
+            msg += '\n{name} -- {doc}'.format(name=k, doc=v.__doc__)
+    elif '[@ME]' in content:  # command dispatcher
+        query = content.strip().replace('[@ME]', '')
+        handler = commands.get(query.split()[0], None)
+        if handler is None:
+            msg = '么得这个功能'
+        else:
+            msg = handler(query, contact, member)
+    elif randint(1, 100) <= 10:
+        time.sleep(2)
+        msg = content + ('（跌坑除外）' if randint(0, 4) == 0 else '')
+
+    bot.SendTo(contact, msg)
+
+
+@register_commands('吃什么')
+def eating(query, contact, member):
+    """随机返回一种吃的"""
+    return "{name}，你阔以选择{meal}".format(
+        name=member.nick, meal=choice(list(meal)))
+
+
+# To add function:
+# use register_commands to register your command handler
+# for each handler, you will get the query string, the contact
+# object and the member object
