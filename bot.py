196a8710fe9a2e7fee13ffc887995504d7063032
yaasdf <rustbell@hotmail.com>
2018-11-15 17:31:54 +0800

    吃什么

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
+# -*- coding:utf-8 -*-
 from random import shuffle
 from os import urandom
 from collections import namedtuple
+from qqbot.utf8logger import ERROR, INFO
 import time
 
 
@@ -25,31 +27,29 @@ MsgTuple = namedtuple('MsgTuple', ['sender', 'time', 'content'])
 
 
 def randint(l, r):
-    return int.from_bytes(urandom(32), 'big') % (r - l) + l
+    value = int.from_bytes(urandom(32), 'big') % (r - l) + l
+    INFO('[randint] [%d ~ %d] 返回了 %d', l, r, value)
+    return value
 
 
 commands = dict()
 history = MsgContainer(50)
 
-meal = [
-    "盖浇饭", "砂锅", "大排档", "米线", "满汉全席", "西餐", "麻辣烫", "自助餐", "炒面", "冒菜", "水果",
-    "西北风", "馄饨", "锅巴饭","麻辣香锅","方便火锅","711","罗森","火锅", "烧烤", "泡面", "水饺", "日料", "涮羊肉",
-    "味千拉面", "面包", "扬州炒饭","隆江猪脚饭","煲仔饭","鸡排饭","石锅饭","三明治","凉面","凉皮","饭团",
-    "茶餐厅", "海底捞", "披萨", "金拱门", "KFC", "汉堡王", "卡乐星", "兰州拉面", "沙县小吃",
-    "烤鱼", "烤肉", "海鲜", "铁板烧", "韩国料理", "粥", "脆皮鸡饭", "萨莉亚", "桂林米粉", "东南亚菜", "甜点",
-    "农家菜", "生活", "全家", "黄焖鸡米饭", "群主", 
-    "虾饺", "斋肠", "牛肉肠", "叉烧肠", "虾肠", "红米肠", "烧卖", "牛肉烧卖", "糯米鸡","珍珠鸡", 
-    "凤爪", "白云凤爪", "蒜蓉排骨", "炸云吞", "炸春卷", "煎虾米肠", "鱿鱼酥", "金钱肚", "叉烧包",
-    "流沙包", "奶黄包", "茅根粥", "皮蛋瘦肉粥", "蛋挞", "椰子布丁", "双皮奶", "马拉糕", "烤田鸡肉", 
-    "蟹煲", "香辣虾", "香辣蟹", "小龙虾", "酸汤肥牛", "椒盐皮皮虾", "芝士年糕", "火锅", 
-    "口水鸡", "藤椒鸡", "周黑鸭", "鸭翅", "鸭脖", "鸭锁骨", "鸭腿", "香辣鳗鱼条", "鱿鱼丝", 
-    "三文鱼刺身", "蒲烧鳗鱼", "帝王蟹柳", "鲜芋仙", "仙草芋圆", "杨枝甘露", "芒果西米", 
-    "寿司食べたい", "拉面", "意大利面", "披萨", "可口可乐", "旺仔牛奶", "板烧", "巨无霸", 
-    "吮指原味鸡", "云吞面", "麻辣烫", "手抓饼", "盆菜", "冒菜", "豆花", "爆浆鸡排", "车仔面", 
-    "叉烧", "烧鹅", "干炒牛河", "田螺", "水饺", "串", "巧克力", "可可脆片", "脱水饼干", 
-    "炒板栗", "龟龄膏", "鸳鸯馒头", "蛋糕", "博多", "蛋挞", "小笼包", "兰州拉面", "冚家便当", 
-    "海底捞", "鸡肉卷"
-])
+meal_def = []
+meal = []
+try:
+    with open('meal_def.txt', 'r') as f:
+        for line in f:
+            meal_def.append(line.strip())
+except FileNotFoundError:
+    INFO('路径出错啦，没有找到默认菜单！')
+
+try:
+    with open('meal.txt', 'r') as f:
+        for line in f:
+            meal.append(line.strip())
+except FileNotFoundError:
+    INFO('路径出错啦，没有找到群友菜单！')
 
 
 def register_commands(command):
@@ -61,41 +61,101 @@ def register_commands(command):
 
 
 def onQQMessage(bot, contact, member, content):
-    if '大床' not in contact.nick or bot.isMe(contact, member):
+
+    # availability filter
+    if bot.isMe(contact, member):
         return
+
+    # 记录
     history.append(
         MsgTuple(
             sender=member.nick,
             time=time.localtime(),
             content=('色图' if len(content) == 0 else content)))
+
+    # command dispatcher
     returnMsg = ''
-    if '帮助' in content:
-        returnMsg += "这是现在可以公布的情报："
-        for k, v in commands.items():
-            returnMsg += '\n{name} -- {doc}'.format(name=k, doc=v.__doc__)
-    elif '[@ME]' in content:  # command dispatcher
-        query = content.strip().replace('[@ME]', '')
+    if '[@ME]' in content:  
+        query = content.replace('[@ME]', '').replace('\n', ' ').replace('\r', '').strip()
         handler = commands.get(query.split()[0], None)
+        time.sleep(1)
         if handler is None:
             returnMsg = '么得这个功能'
         else:
             returnMsg = handler(query, contact, member)
-    elif randint(1, 100) <= 5:
-        time.sleep(2)
-        returnMsg = content + ('（跌坑除外）' if randint(0, 4) == 0 else '')
 
+    # 跌坑除外
+    #elif randint(1, 100) <= 5:
+    #    time.sleep(2)
+    #    returnMsg = content + ('（跌坑除外）' if randint(0, 4) == 0 else '')
+
+    # return
     if returnMsg is not None and len(returnMsg) != 0:
         bot.SendTo(contact, returnMsg)
 
 
+@register_commands('帮助')
+def help(query, contact, member):
+    """显示帮助"""
+    returnMsg = "这是目前可以公布的情报："
+    for k, v in commands.items():
+        if k != '帮助':
+            returnMsg += '\n{name} -- {doc}'.format(name=k, doc=v.__doc__)
+    returnMsg += '\n现在一共有{total}种菜！包括默认菜单{fb}味，群友提供{custom}味'.format(total=len(meal_def)+len(meal), fb=len(meal_def), custom=len(meal))
+    return returnMsg
+
 @register_commands('吃什么')
 def eating(query, contact, member):
     """随机返回一种吃的"""
-    shuffle(meal)
-    return "{name}，你阔以选择{meal}".format(name=member.nick, meal=meal[0])
+    if len(meal_def) + len(meal) == 0:
+        return "菜卖完勒，求你加菜！"
+    else:
+        idx = randint(0, len(meal_def) + len(meal))
+        if idx < len(meal_def):
+            return "{name}，你阔以选择{meal}".format(name=member.name, meal=meal_def[idx])
+        elif idx < len(meal_def) + len(meal):
+            return "{name}，你阔以选择{meal}".format(name=member.name, meal=meal[idx - len(meal_def)])
 
 
-@register_commands('撤回')
+food_blacklist = [
+        '你妈',
+        '你媽',
+        '你母亲',
+        '屎',
+        '粪',
+        '骨灰',
+        '哈哈哈',
+        'shit',
+        'Shit',
+        'SHIT',
+        'poop',
+        '便便',
+        '什么',
+        '提供',
+        '哈哈'
+        ]
+
+@register_commands('加菜')
+def eating(query, contact, member):
+    """添加吃什么菜单"""
+    food = query.split(' ', 1)[1]
+
+    if food == '':
+        return "空气不能吃的"
+
+    if '/表情' in food:
+        return "bot看不懂表情"
+
+    for fb in food_blacklist:
+        if fb in food:
+            return "你加你妈呢"
+
+    meal.append(food + " ({name}提供)".format(name=member.name))
+    with open('meal.txt', 'a') as f:
+        f.write(meal[-1] + '\n')
+    return "已添加{meal}".format(meal=meal[-1])
+
+#@register_commands('撤回')
 def undo(query, contact, member):
     """返回撤回消息，格式：撤回 消息位置"""
     try:
